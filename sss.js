d94d6f68358d59669506887f895efb7d0ca022cf
Alex_PgDn <3alexn3@gmail.com>
2024-09-03 16:51:16 +0300

    New one comm

--- sss.js
+++ sss.js
@@ -1,1 +1,4 @@
-alert("hello world");
+let people = Number(prompt("Number of people?"));
+let price = Number(prompt("Cost of food"));
+let percent = Number(prompt("Percent"))
+alert(`${price+(price*percent/100)/people}`)
