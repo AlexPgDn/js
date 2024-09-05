c35d9fc3ff54c0ee0dcb9a617e524ad1e4c9b3e7
Alex_PgDn <3alexn3@gmail.com>
2024-09-05 19:30:16 +0300

    Another business4later

--- sss.js
+++ sss.js
@@ -10,17 +10,16 @@ function calculatePerPersonAmount(totalAmount, numberOfPeople) {
     return totalAmount / numberOfPeople;
 }
 
-// Получаем ссылку на кнопку
+
 var calculateButton = document.getElementById("calculateButton");
 
-// Назначаем обработчик события на кнопку
 calculateButton.addEventListener("click", function () {
-    // Получаем значения из полей ввода
+   
     var billAmount = parseInt(document.getElementById("billAmount").value);
     var tipPercentage = parseInt(document.getElementById("tipPercentage").value);
     var numberOfPeople = parseInt(document.getElementById("numberOfPeople").value);
 
-    // Проверяем, все ли поля заполнены корректно
+   
     if (isNaN(billAmount)
         || isNaN(tipPercentage)
         || isNaN(numberOfPeople)
@@ -31,14 +30,12 @@ calculateButton.addEventListener("click", function () {
         return;
     }
 
-    // Вычисляем чаевые и общую сумму
+  
     var tipAmount = calculateTipAmount(billAmount, tipPercentage);
     var totalAmount = calculateTotalAmount(billAmount, tipAmount);
 
-    // Вычисляем сумму для каждого человека
     var perPersonAmount = calculatePerPersonAmount(totalAmount, numberOfPeople);
 
-    // Выводим результаты
     document.getElementById("tipAmount").innerText = tipAmount.toFixed(2);
     document.getElementById("totalAmount").innerText = totalAmount.toFixed(2);
     document.getElementById("perPersonAmount").innerText = perPersonAmount.toFixed(2);
